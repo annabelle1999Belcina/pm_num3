bb9a85029df6c06f99823e3be6a90857c0e1ba3a
Heavenjes <paulinecalatrava@gmail.com>
2019-09-28 19:16:41 +0800

    initial commit

---
+++ src/components/Failed.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+// import Login from './Login';
+
+class Failed extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Invalid Credentials!</h1>
+            </div>
+        )
+    }
+}
+export default Failed;
